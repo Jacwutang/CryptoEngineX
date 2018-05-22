ea8001de63eaf6f0bc771682037497bb53d14c5b
Jack Wu <jackwu350@yahoo.com>
2018-05-22 11:16:26 -0700

    increased # of trades to display o 15

--- client/src/components/Transaction/utils.js
+++ client/src/components/Transaction/utils.js
@@ -2,7 +2,7 @@ const ccxt = require('ccxt');
 
 let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export async function getTrades(market, exchange, limit = 10) {
+export async function getTrades(market, exchange, limit = 15) {
   exchange = new ccxt[exchange]({
     proxy: 'https://cors-anywhere.herokuapp.com/',
     timeout: 10000,
