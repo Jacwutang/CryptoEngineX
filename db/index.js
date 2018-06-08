b41527892827d7eb2bcda82cd3da7fc8fbf942fb
Jack Wu <jackwu350@yahoo.com>
2018-06-07 22:09:09 -0700

    add knex sql query builder module

--- db/index.js
+++ db/index.js
@@ -1,12 +1,22 @@
 //PostgreSQL setup
-const pgp = require("pg-promise")();
-const connectionString =
-  process.env.ELEPHANTSQL_URL ||
-  "postgres//localhost:5432/CryptoEngineX_development";
-const db = pgp(connectionString);
+// const pgp = require("pg-promise")();
+// const connectionString =
+//   process.env.ELEPHANTSQL_URL ||
+//   "postgres//localhost:5432/CryptoEngineX_development";
+// const db = pgp(connectionString);
 
 // module.exports = {
 //   query: (text, params, callback) => db.query(text, params, callback)
 // };
+
+const connectionString =
+  process.env.PG_CONNECTION_STRING ||
+  "postgres//localhost:5432/CryptoEngineX_development";
+
+const db = require("knex")({
+  client: "pg",
+  connection: connectionString,
+  debug: true
+});
 
 module.exports = db;
